d9985e0d283bde2a227791c121e4e132520c3896
Johan Mattisson <johan.mattisson@gmail.com>
2021-06-13 16:24:54 +0200

    Added Tailwind CSS with some basic configurations

---
+++ assets/tailwind.config.js
@@ -0,0 +1,30 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === "production",
+    content: [
+      "../lib/**/*.eex",
+      "../lib/**/*.leex",
+      "../lib/**/*_view.ex"
+    ],
+    options: {
+      whitelist: [/phx/, /nprogress/]
+    }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderWidth: ['hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
